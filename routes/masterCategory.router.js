b0723b76b34f0bf5a27494f9647926a6f10262e7
Musheeb <museebnoorisys240@gmail.com>
2026-05-30 20:04:42 +0530

    feat: get categories API created

--- routes/masterCategory.router.js
+++ routes/masterCategory.router.js
@@ -1,7 +1,11 @@
 const auth = require("../middlewares/auth.middleware");
 
-const { addCategory } = require("../controllers/category.controller");
+const {
+  addCategory,
+  getCategories,
+} = require("../controllers/category.controller");
 
 module.exports = async (app) => {
   app.post("/add-category", auth, addCategory);
+  app.get("/get-categories", auth, getCategories);
 };
--- services/masterCategory.services.js
+++ services/masterCategory.services.js
@@ -15,3 +15,13 @@ exports.getByName = async (query) => {
     throw e;
   }
 };
+
+exports.getAllCategories = async (userId) => {
+  try {
+    return await CategoryModel.find({ addedBy: userId, isActive: true }).select(
+      "_id name isActive addedBy createdAt",
+    );
+  } catch (e) {
+    throw e;
+  }
+};
