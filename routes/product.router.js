ade8ab5fdb91716d74ee336e537ae733da9b5a6b
Musheeb <museebnoorisys240@gmail.com>
2026-06-10 00:14:14 +0530

    feat: get products list API skeleton

--- routes/product.router.js
+++ routes/product.router.js
@@ -1,7 +1,11 @@
 const auth = require("../middlewares/auth.middleware");
-const { addProducts } = require("../controllers/product.controller");
+const {
+  addProducts,
+  getProductsList,
+} = require("../controllers/product.controller");
 const upload = require("../middlewares/multer.middleware");
 
 module.exports = async (app) => {
   app.post("/add-product", auth, upload.array("images", 4), addProducts);
+  app.get("/get-products-list", auth, getProductsList);
 };
