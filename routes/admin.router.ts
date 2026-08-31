2d79fbec8eaa2521ecbf7599755b85ebe623b7a9
Musheeb <museebnoorisys240@gmail.com>
2026-08-31 22:05:16 +0530

    feat: low stock and new arrival count

--- routes/admin.router.ts
+++ routes/admin.router.ts
@@ -1,3 +1,5 @@
+import ProductModel from "../models/Product.model";
+
 import { Application, Request, Response, NextFunction } from "express";
 
 module.exports = async (app: Application) => {
@@ -15,11 +17,23 @@ module.exports = async (app: Application) => {
     "/get-statistics",
     async (req: Request, res: Response, next: NextFunction) => {
       try {
+        const currentTime = new Date();
+        const before24Hours = currentTime.getTime() - 24 * 60 * 60 * 1000;
+        const LOW_STOCK_LIMIT = Number(process.env.LOW_STOCK_LIMIT) || 5;
         // Get new arrivals.
+        const newArrivalsCount = await ProductModel.countDocuments({
+          createdAt: { $gte: before24Hours },
+        });
 
         //Get low stock count.
+        const lowStockCount: number = await ProductModel.countDocuments({
+          quantity: { $lte: LOW_STOCK_LIMIT },
+        });
 
-        return res.send("Dashboard statistics API is working ✅");
+        return res.status(200).json({
+          lowStockCount,
+          newArrivalsCount,
+        });
       } catch (e) {
         next(e);
       }
